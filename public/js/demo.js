e51731e0d1e3e170cae6d75be733a4ea08321720
Mihail Eremkin <developer@skm-mobile.com>
2022-09-30 10:00:50 +0300

    Experiment 1

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,28 +1,27 @@
-TrelloPowerUp.initialize({
-    "card-buttons": function (t, options) {
-        return [
-            // {
-            //     icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-            //     text: "GitLab",
-            //     callback: function (t) {
-            //         return t.popup({
-            //             title: "Estimation",
-            //             url: "estimate.html",
-            //         });
-            //     },
-            // },
-            {
-                icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-                text: "Summary",
-                callback: copySummary,
-            }
-        ];
-    },
-});
+TrelloPowerUp.initialize(
+    {
+        "card-buttons": function (t, options) {
+            return [
+                {
+                    icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
+                    text: "Summary",
+                    //callback: copySummary,
+                    callback: function (t) {
+                        return t.popup({
+                                title: "Simple popup"
+                            }
+                        )
+                    }
+                }
+            ];
+        },
+    });
 
 const copySummary = function (t) {
     const text = "XXXXX"
-    setTimeout(function() {copyTextToClipboard(text)}, 3000)
+    setTimeout(function () {
+        copyTextToClipboard(text)
+    }, 3000)
 }
 
 function copyTextToClipboard(text) {
