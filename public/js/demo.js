a3faf2134b6d1db3b78c3a1ceb279e2ac6418fd5
Mihail Eremkin <developer@skm-mobile.com>
2022-09-30 09:39:32 +0300

    Experiment 1

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,21 +1,21 @@
 TrelloPowerUp.initialize({
     "card-buttons": function (t, options) {
         return [
-            {
-                icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-                text: "GitLab",
-                callback: function (t) {
-                    return t.popup({
-                        title: "Estimation",
-                        url: "estimate.html",
-                    });
-                },
-            },
+            // {
+            //     icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
+            //     text: "GitLab",
+            //     callback: function (t) {
+            //         return t.popup({
+            //             title: "Estimation",
+            //             url: "estimate.html",
+            //         });
+            //     },
+            // },
             {
                 icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
                 text: "Summary",
                 callback: copySummary,
-            },
+            }
         ];
     },
 });
@@ -23,7 +23,7 @@ TrelloPowerUp.initialize({
 const copySummary = function (t) {
     const text = "XXXXX"
     setTimeout(function() {copyTextToClipboard(text)}, 3000)
-};
+}
 
 function copyTextToClipboard(text) {
     const textArea = document.createElement("textarea");
